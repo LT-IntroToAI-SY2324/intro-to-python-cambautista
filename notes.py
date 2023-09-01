342f9272c667635e8dfa3506ada9f717f36c575a
Camila <cbautista24@cps.edu>
2023-09-01 11:23:10 -0500

    added

--- notes.py
+++ notes.py
@@ -2,7 +2,7 @@
 # There is a lot in this file, you should add, subtract, change things as needed to help fully understand the process.
 # Mr. Berg will be going over all parts, but will not be grading anything in this file
 
-# types
+types
 
 # str
 y = "I love Computer Science!"
@@ -25,18 +25,18 @@ z = lane_tech.pop()
 print(z)
 
 
-# Indices
-print(lane_tech[1])
-print(lane_tech[-1])
-print(lane_tech[len(lane_tech)-1])
+# # Indices
+# print(lane_tech[1])
+# print(lane_tech[-1])  #to get the last element on the list
+# print(lane_tech[len(lane_tech)-1]) #len=length of list
 
-# Slices
-print(lane_tech[1:3])
-print(lane_tech[:3])
-print(lane_tech[1:])
-print(lane_tech[:])
+# # Slices -one piece of the list
+# print(lane_tech[1:3]) # returns a part of the list from 1-3
+# print(lane_tech[:3]) #0 is the default 0-3
+# print(lane_tech[1:]) #1-the whole thing 
+# print(lane_tech[:]) #from 0 to the end 
 
-# Functions
+# # Functions
 
 # defining a function
 def hello_world():
@@ -59,43 +59,50 @@ def add_two(n: int):
 assert add_two(5) == 7, "add_two with input 5 test"
 assert add_two(-2) == 0, "add_two with input -2 test"
 assert add_two(0) == 2, "add_two with input 0 test"
-
-# Loops
-
-# For Loop Template 1
-# do something for each item in a list
-lst = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-for el in lst:
-    print(el)
-
-# For Loop Template 2
-# do something n times
-for i in range(5):
-    print("Intro to AI")
-
-# For Loop Template 3
-# do something for each item in a list but we care about the index
-for r in range(len(lst)):
-    if lst[r] == 'b':
-        lst[r] = 'c'
-
-print(lst)
-
-# Dictionaries
-
-dict = {"name": "rob", "age": 30}
-print(dict["age"])
-
-# Random
-import random
-
-food = ['pizza', 'carrots', 'eggs']
-dinner = random.choice(food)
-print(dinner)
-
-# f String
-age = 25
-# print("Mary is " + age + " years old")
-print(f"Mary is {age} years old")
+#asserts are tests that ensure if things are correct
+# ex: 2 asserts, as soon as it fails one test it cuts the program off
 
 
+# # Loops
+
+# # For Loop Template 1
+# # do something for each item in a list
+# lst = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+# for el in lst:
+#     print(el)
+
+# # For Loop Template 2
+# # do something n times
+# for i in range(5):
+#     print("Intro to AI")
+
+# # For Loop Template 3
+# # do something for each item in a list but we care about the index
+# for r in range(len(lst)):
+#     if lst[r] == 'b':
+#         lst[r] = 'c'
+
+# print(lst)
+
+# # Dictionaries
+
+# dict = {"name": "rob", "age": 30}
+# print(dict["age"])
+
+# # Random
+# import random
+
+# food = ['pizza', 'carrots', 'eggs']
+# dinner = random.choice(food)
+# print(dinner)
+
+# # f String
+# age = 25
+# # print("Mary is " + age + " years old")
+# print(f"Mary is {age} years old")
+
+
+# control+shift then down arrow to select and scroll
+# downside of java: performance issues
+# control + fowardslash to uncomment
+# multiline comments use triple quotes
